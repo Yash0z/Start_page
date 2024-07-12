db90b4c2fb9eb9e0e99fcb7218f11b32a8d96f9a
kai <yash.amberkar005@gmail.com>
2024-07-12 20:32:18 +0530

    card function

--- src/Components/Workspace.jsx
+++ src/Components/Workspace.jsx
@@ -2,32 +2,32 @@ import React, { useEffect, useRef } from "react";
 import Card from "./Card";
 
 const Workspace = () => {
-   //shortcuts
-	const list = [
-		<Card key={1} />,
-		<Card key={2} />,
-		<Card key={3} />,
-		<Card key={4} />,
-		<Card key={5} />,
-		<Card key={6} />,
-		<Card key={7} />,
-		<Card key={8} />,
+	// Array of unique props for each Card
+	const cardsData = [
+		{ id: 1, title: "Card 1", link: "https://github.com/" },
+		{ id: 2, title: "Card 2", link: "Content for Card 2" },
+		{ id: 3, title: "Card 3", link: "Content for Card 3" },
+		{ id: 4, title: "Card 4", link: "Content for Card 4" },
+		{ id: 5, title: "Card 5", link: "Content for Card 5" },
+		{ id: 6, title: "Card 6", link: "Content for Card 6" },
+		{ id: 7, title: "Card 7", link: "Content for Card 7" },
+		{ id: 8, title: "Card 8", link: "Content for Card 8" },
 	];
 
-   //scrollbar behaviour
+	// scrollbar behaviour
 	const containerRef = useRef(null);
 	useEffect(() => {
 		const container = containerRef.current;
 
 		const handleScroll = (event) => {
-			event.preventDefault(); 
+			event.preventDefault();
 
 			if (event.deltaY < 0) {
 				// Scroll up, scroll right
-				container.scrollLeft += 50; 
+				container.scrollLeft += 50;
 			} else {
 				// Scroll down, scroll left
-				container.scrollLeft -= 50; 
+				container.scrollLeft -= 50;
 			}
 		};
 
@@ -37,8 +37,6 @@ const Workspace = () => {
 			container.removeEventListener("wheel", handleScroll);
 		};
 	}, []);
-
-
 
 	return (
 		<div className='relative flex flex-col border h-[20%] w-[90vw] md:max-w-screen-xl md:max-h-32 mx-auto m-5 p-4 md:flex-row md:justify-between'>
@@ -52,9 +50,9 @@ const Workspace = () => {
 				className='border-2 flex-wrap flex-col flex space-x-7 border-red-500 p-3 my-auto h-[100%] md:h-[80%] w-[100%] overflow-x-auto md:w-[75%] custom-scrollbar'
 				ref={containerRef}
 			>
-				{list.map((item, index) => (
-					<div key={index} className='border w-52 h-10'>
-						{item}
+				{cardsData.map((item) => (
+					<div key={item.id} className='border w-52 h-10'>
+						<Card {...item} />
 					</div>
 				))}
 			</div>
