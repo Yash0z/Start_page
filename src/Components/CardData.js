48032820c57289d6eed53518e6d94f1f18a199cd
kai <yash.amberkar005@gmail.com>
2024-07-15 12:12:02 +0530

    added CardManager

---
+++ src/Components/CardData.js
@@ -0,0 +1,14 @@
+export const cardsData1 = [
+   { id: 1, title: "Card 1", link: "https://github.com/" },
+   { id: 2, title: "Card 2", link: "Content for Card 2" },
+];
+
+export const cardsData2 = [
+   { id: 1, title: "Card 3", link: "Content for Card 3" },
+   { id: 2, title: "Card 4", link: "Content for Card 4" },
+];
+
+export const cardsData3 = [
+   { id: 1, title: "Card 5", link: "Content for Card 5" },
+   { id: 2, title: "Card 6", link: "Content for Card 6" },
+];
---
+++ src/Components/CardsManager.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const CardsManager = () => {
+  return (
+    <div>CardsManager</div>
+  )
+}
+
+export default CardsManager
