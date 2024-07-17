5cf0833f1c0461ad2e1744c7ab0a35ebd21b56b6
kai <yash.amberkar005@gmail.com>
2024-07-17 12:18:30 +0530

    Updated Card Design

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,15 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
+import { RiDeleteBin6Fill } from "@react-icons/all-files/ri/RiDeleteBin6Fill";
+import { MdCancel } from "@react-icons/all-files/md/MdCancel";
 
-const Card = ({ title, link }) => {
+const Card = ({ id, title, link, onDelete }) => {
+	const [isVisible, setIsVisible] = useState(true);
+
+	const handleContextMenu = (e) => {
+		e.preventDefault();
+		setIsVisible(false);
+	};
+
+	const handleDeleteClick = () => {
+		onDelete(id);
+	};
+
+	const handleCancelClick = () => {
+		setIsVisible(true);
+	};
+
 	return (
 		<>
-			<div className=' relative  flex h-full w-full justify-between '>
-				<a href={link}>
-					<span className=' absolute text-center content-center w-[100%] h-[100%] text-red-50 '>
-						{title}
-					</span>
-				</a>
-			</div>
+			{isVisible ? (
+				<div
+					onContextMenu={handleContextMenu}
+					className='font-Questrial text-xl rounded-sm  text-white border text-center  font-bold card-container relative h-full w-full'
+				>
+					<a href={link} className=' content-center block w-full h-full'>
+						~ {title}
+					</a>
+				</div>
+			) : (
+				<div className='flex justify-center gap-2 text-yellow-400 border w-full h-full '>
+					<button onClick={handleDeleteClick} aria-label='Delete'>
+						<RiDeleteBin6Fill size={20} />
+					</button>
+					<button onClick={handleCancelClick} aria-label='Cancel'>
+						<MdCancel size={20} />
+					</button>
+				</div>
+			)}
 		</>
 	);
 };
