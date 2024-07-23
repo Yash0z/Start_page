f6321110acacb7699310192f8dcce5869efb7022
kai <yash.amberkar005@gmail.com>
2024-07-23 21:16:14 +0530

    optimized

--- src/Components/Popup.jsx
+++ src/Components/Popup.jsx
@@ -4,40 +4,38 @@ const Popup = ({ addCard, isOpen }) => {
 	const [name, setName] = useState("");
 	const [url, setUrl] = useState("");
 	const [selectedWorkspace, setSelectedWorkspace] = useState("data1");
-
+	const options = [
+		{ value: "data1", label: "Workspace 1" },
+		{ value: "data2", label: "Workspace 2" },
+		{ value: "data3", label: "Workspace 3" },
+	];
 	const handleNameChange = (event) => {
 		setName(event.target.value);
 	};
-
-	const handleUrlChange = (event) => {
+   const handleUrlChange = (event) => {
 		setUrl(event.target.value);
 	};
 
-	const handleWorkspaceChange = (event) => {
-		setSelectedWorkspace(event.target.value);
-	};
 
 	const handleSubmit = () => {
-		addCard(name, url, selectedWorkspace); // Pass name, url, and selectedWorkspace to addCard function
+		addCard(name, url, selectedWorkspace); 
 		setName("");
 		setUrl("");
-		isOpen(false); // Call isOpen function to close the popup/modal (assuming it accepts a boolean)
+		isOpen(false); 
 	};
 
 	const handleClose = () => {
-		isOpen(false); // Call isOpen function to close the popup/modal
+		isOpen(false); 
 	};
 
 	return (
 		<>
 			<div className='fixed inset-0 bg-black bg-opacity-90 '>
-				<div className='text-white relative top-10 flex flex-col items-center mx-auto w-[40rem] h-[19rem] '>
+				<div className='text-white relative top-10 flex flex-col items-center mx-auto w-[40rem] h-[18rem] '>
 					<div className='flex flex-col gap-5 p-5 font-Euclid_re text-xl text-accent2 '>
 						{/* Name input */}
-						<label className="flex p-2 items-center gap-5 ">
-							<span>
-								Title
-							</span>
+						<label className='flex p-2 items-center gap-5 '>
+							<span>Title</span>
 							<div>
 								<input
 									type='text'
@@ -50,10 +48,8 @@ const Popup = ({ addCard, isOpen }) => {
 						</label>
 
 						{/* URL input */}
-						<label className="flex p-2 items-center gap-5 ">
-							<span >
-								URL
-							</span>
+						<label className='flex p-2 items-center gap-5 '>
+							<span>URL</span>
 							<div>
 								<input
 									type='text'
@@ -66,29 +62,34 @@ const Popup = ({ addCard, isOpen }) => {
 						</label>
 
 						{/* Workspace select */}
-						<label className=" p-2 flex  flex-col gap-1 w-[45%]  ">
-							<span >
-								Workspace
-							</span>
+						<label className=' p-2 flex  flex-col gap-1 w-[45%]  '>
+							<span>Workspace</span>
 							<div>
 								<Select
-                        variant="standard"
-                            
-                            placeholder={"select"}
+									variant='standard'
+									placeholder={"select"}
 									className='z-10 absolute  border-accent2   text-textclr2 text-sm  w-[15rem]   '
 									value={selectedWorkspace}
-									onChange={handleWorkspaceChange}
+									onChange={(e) => {
+										setSelectedWorkspace(e);
+									}}
 								>
-									<Option value='data1'>Workspace 1</Option>
-									<Option value='data2'>Workspace 2</Option>
-									<Option value='data3'>Workspace 3</Option>
+									{options.map((option, index) => (
+										<Option
+											key={index}
+											className='bg-white  hover:text-accent1'
+											value={option.value}
+										>
+											{option.label}
+										</Option>
+									))}
 								</Select>
 							</div>
 						</label>
 					</div>
 
 					{/* Submit and Cancel buttons */}
-					<div className='flex justify-end gap-2 h-10 bg-transparent   w-full px-12 absolute bottom-14 text-black'>
+					<div className='flex justify-end gap-2 h-10 bg-transparent   w-full px-12 absolute bottom-10 text-black'>
 						<button
 							onClick={handleClose}
 							className='bg-accent1 px-3 rounded-sm'
--- test.sh
+++
@@ -1,4 +0,0 @@
-#!/bash/sh
-
-echo "Top 10 process:"
-ps aux | head -10
